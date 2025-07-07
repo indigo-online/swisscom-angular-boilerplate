b8a5d25af2b5bf646047c92d80147b9d80ed4797
sandrooco <sandro.colombo.sc@gmail.com>
2025-07-07 15:52:11 +0200

    docs: add comment to which client is used

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,3 +1,3 @@
 export const environment = {
-  clientId: '16YfakqXn9HBI6qspnMurLz2AOjdMId0y6UxfHJgArBVZkX2gZfOwnxAqVIMDBmu',
+  clientId: '16YfakqXn9HBI6qspnMurLz2AOjdMId0y6UxfHJgArBVZkX2gZfOwnxAqVIMDBmu', // based on https://dev-scapp-console.swisscom.com/dashboard/service/46c0bfde-ecfa-4827-b6c8-cca8f084c701/service-keys (ECLD dev space)
 };
