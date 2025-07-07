5788a23b7465bb5ff1c1bcbdf083bb73c429b8bf
sandrooco <sandro.colombo.sc@gmail.com>
2025-07-07 16:21:06 +0200

    fix: rename exampel/example

--- src/components/example/example.component.ts
+++ src/components/example/example.component.ts
@@ -10,21 +10,21 @@ import { OidcSecurityService } from 'angular-auth-oidc-client';
 import { CorpIdUserData } from '../../app/auth/corpid.type';
 
 @Component({
-  selector: 'app-exampel',
-  templateUrl: './exampel.component.html',
+  selector: 'app-example',
+  templateUrl: './example.component.html',
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
-export class ExampelComponent {
+export class ExampleComponent {
   private readonly oidcSecurityService = inject(OidcSecurityService);
 
   protected showText = signal(false);
   protected userData = computed(
-    () => this.oidcSecurityService.userData().userData as CorpIdUserData
+    () => this.oidcSecurityService.userData().userData as CorpIdUserData,
   );
 
   // example for a backend call with automated fetch-handling
   protected userFromServer = httpResource(
-    () => `https://localhost:4000/api/user/${this.userData().user_name}`
+    () => `https://localhost:4000/api/user/${this.userData().user_name}`,
   );
 
   login() {
